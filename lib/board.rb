2020670e06cc3db3ddc7b5792cf5f133f7722442
KateWilkinson <kate.wilkinson@hotmail.com>
2015-06-17 11:41:58 +0100

    Added board array and method add_ship; added test to check if ship can be hit

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,15 @@
+require_relative 'ship'
+
 class Board
 
+  attr_reader :board
+
+  def initialize
+    @board = []
+  end
+
+  def add_ship ship
+    @board << ship
+  end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,11 +2,20 @@ require 'board'
 
 describe Board do
 
+  let(:ship) {double :ship}
+  before(:each) do
+    board = Board.new
+    subject.add_ship(ship)
+  end
+
+
   it 'can have a ship' do
-    ship = double :ship
-    board = []
-    subject.add_ship
     expect(subject.board).to include(ship)
+  end
+
+  it 'can receive a hit on a ship' do
+    subject.shoot_at(position)
+    expect(subject.board).not_to include(ship)
   end
 
 end
