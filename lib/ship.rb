c2f805928d85e9527e1c43fdf9027dc58fe1041f
MassimilianoMura <massimilianomura1983@gmail.com>
2015-06-17 17:55:34 +0100

    created hit method and updated sunk?

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,16 +1,21 @@
 class Ship
 
   attr_reader :position, :size
-  attr_accessor :hits
+  attr_accessor :hits, :sunk
 
   def initialize position, size
     @position = position
     @size = size
     @hits = 0
+    @sunk = false
   end
 
   def sunk?
-    hits == size ? true : false
+    hits >= size ? (@sunk = true) : (@sunk = false)
+  end
+
+  def hit
+    @hits += 1
   end
 
 
