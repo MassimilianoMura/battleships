5f831818626674e210e303033158e90262a1c2a0
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-06-18 14:50:47 +0100

    create test for E and W directions

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -21,36 +21,48 @@ require 'ship'
       expect(ship.hits).to eq 0
     end
 
-  describe 'add_position' do
+    describe 'add_position' do
 
-    let(:ship) { Ship.new 'C4' , 2 , 'N' }
-    it 'adds coordinate values based on north direction' do
-      ship.add_position
-      expect(ship.all_positions).to include 'C3'
+      let(:ship) { Ship.new 'C4' , 2 , 'N' }
+      it 'adds coordinate values based on north direction' do
+        ship.add_position
+        expect(ship.all_positions).to include 'C3'
+      end
+
+      let(:ship2) { Ship.new 'C4' , 2 , 'S' }
+      it 'adds coordinate values based on south direction' do
+        ship2.add_position
+        expect(ship2.all_positions).to include 'C5'
+      end
+
+      let(:ship3) { Ship.new 'C4' , 2 , 'E' }
+      it 'adds coordinate values based on south direction' do
+        ship2.add_position
+        expect(ship2.all_positions).to include 'D4'
+      end
+
+      let(:ship4) { Ship.new 'C4' , 2 , 'W' }
+      it 'adds coordinate values based on south direction' do
+        ship2.add_position
+        expect(ship2.all_positions).to include 'B4'
+      end
+
     end
 
-    let(:ship2) { Ship.new 'C4' , 2 , 'S' }
-    it 'adds coordinate values based on south direction' do
-      ship2.add_position
-      expect(ship2.all_positions).to include 'C5'
+
+    describe 'hit' do
+      it 'increases the value of hits when it is hit' do
+        ship.receive_hit
+        expect(ship.hits).to eq 1
+      end
+
     end
 
-  end
-
-
-  describe 'hit' do
-    it 'increases the value of hits when it is hit' do
-      ship.receive_hit
-      expect(ship.hits).to eq 1
+    describe 'sunk?' do
+      it 'checks if the ship is sunk' do
+        expect(ship.sunk?).to be false
+      end
     end
-
-  end
-
-  describe 'sunk?' do
-    it 'checks if the ship is sunk' do
-      expect(ship.sunk?).to be false
-    end
-  end
 
 
 
