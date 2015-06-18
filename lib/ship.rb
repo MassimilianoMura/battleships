ac5c17644039f18c7af3b57ffeb63eb3e1a7e994
MassimilianoMura <massimilianomura1983@gmail.com>
2015-06-18 12:38:26 +0100

    updates

--- lib/ship.rb
+++ lib/ship.rb
@@ -11,6 +11,32 @@ class Ship
     @sunk = false
   end
 
+  def add_position
+    all_position = []
+
+    if self.direction == 'N'
+      @size.times do
+        num ||= get_number_coordinates
+        num -= 1
+        num.to_s
+        let = get_letter_coordinates
+        p num
+        p let
+    end
+  end
+
+  end
+
+  def get_letter_coordinates
+    position[0]
+  end
+
+  def get_number_coordinates
+    position[1].to_i
+  end
+
+
+
   def sunk?
     hits >= size ? (@sunk = true) : (@sunk = false)
   end
